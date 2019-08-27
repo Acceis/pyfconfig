9808841d22255b2457770150f39510a97093bd99
Virgile Jarry <virgilejarry@mailbox.org>
2019-08-27 09:46:37 +0200

    Changes version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfconfig",
-    version="0.1.1",
+    version="0.1.2",
     author="Virgile Jarry",
     author_email="virgilejarry@mailbox.org",
     description="A python library to manage your network interfaces under linux",
